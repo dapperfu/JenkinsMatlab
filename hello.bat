3ff4727b10cf525198f852cb5ac5f6b1fddaedfd
Jed Frey <github+mlrunner@exstatic.org>
2016-12-01 16:05:24 -0500

    Added batch script to run hello.m

---
+++ hello.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\MATLAB\R2016a\bin\matlab.exe" -nosplash -wait -c "C:\Program Files\MATLAB\R2016a\licenses\network.lic" -logfile "hello.log" -r "hello"
--- hello.m
+++ hello.m
@@ -1,1 +1,2 @@
 fprintf('World\n');
+exit;
